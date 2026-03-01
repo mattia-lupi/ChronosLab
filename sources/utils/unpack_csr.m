be6685dba39210586052b60bea0bac9d2e3b313c
Mattia Lupi <m.lupi@m3eweb.it>
2026-03-01 08:50:22 +0100

    improved unpack csr as it could have taken 30% of simulation time in fluids

--- sources/utils/unpack_csr.m
+++ sources/utils/unpack_csr.m
@@ -1,26 +1,17 @@
 function [iat,ja,coef] = unpack_csr(matrix)
 
-[irow,ja,coef] = find(matrix);
-[irow,perm] = sort(irow);
-ja = ja(perm);
-coef = coef(perm);
+   % 1. Transpose to switch from CSC (MATLAB default) to CSR ordering
+   % find() on the transpose iterates over rows of the original matrix sequentially.
+   [ja, irow, coef] = find(matrix.');
 
-nn = size(matrix,1);
-nt = nnz(matrix);
-iat = ones(nn+1,1);
-irow_old = 0;
-for k = 1:nt
-   irow_new = irow(k);
-   if  irow_new > irow_old
-      for j = irow_old+1:irow_new
-         iat(j) = k;
-      end
-      irow_old = irow_new;
-   end
-end
-k = nt+1;
-for j = irow_old+1:nn+1
-   iat(j) = k;
-end
+   % 2. Vectorized construction of row pointers (iat)
+   nn = size(matrix, 1);
+
+   % Count non-zeros per row. accumarray handles empty rows automatically (returns 0).
+   row_counts = accumarray(irow, 1, [nn, 1]);
+
+   % Cumulative sum to generate pointers.
+   % Prepend 1 because CSR pointers are 1-based in this context.
+   iat = [1; cumsum(row_counts) + 1];
 
 end
