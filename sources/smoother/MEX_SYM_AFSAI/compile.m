79adab2d1bb254adbca235af0a2fdd25129c7a9f
Mattia Lupi <m.lupi@m3eweb.it>
2026-03-10 22:22:04 +0100

    further split of the sym FSAI to possibly add stuff later for symmetrising pattern

--- sources/smoother/MEX_SYM_AFSAI/compile.m
+++ sources/smoother/MEX_SYM_AFSAI/compile.m
@@ -1,1 +1,1 @@
-mex -R2018a compute_local_fsai.cpp CXXFLAGS="-fopenmp -fPIC" LDFLAGS="-fopenmp -z noexecstack" -lmwblas -lmwlapack
+mex -R2018a SYM_aFSAI_compute.cpp compute_local_fsai.cpp cpt_afsai_coef.cpp CXXFLAGS="-fopenmp -fPIC" LDFLAGS="-fopenmp -z noexecstack" -lmwblas -lmwlapack
