6d27309e0b48daa1d092da6526ff5b556e66ca44
Andrea Franceschini <franc90@dmsa.unipd.it>
2026-03-18 15:30:43 +0100

    Removed unused file

--- sources/Preconditioner/AMG/prolong/EMIN/Cpp_EMIN/include/spmatv.h
+++
@@ -1,3 +0,0 @@
-void spmatv(const int np, const int nn, const int* __restrict__ iat,
-            const int* __restrict__ ja, const double* __restrict__ coef,
-            const double* __restrict__ v_in, double* __restrict__ v_out);
